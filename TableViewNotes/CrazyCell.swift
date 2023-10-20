958add0d2ceba36d5034564d1607cce6abefe772
Jake Kenefick <jake.kenefick24@d155.org>
2023-10-20 09:40:18 -0500

    1st

---
+++ TableViewNotes/CrazyCell.swift
@@ -0,0 +1,17 @@
+//
+//  CrazyCell.swift
+//  TableViewNotes
+//
+//  Created by JAKE KENEFICK on 10/19/23.
+//
+
+import Foundation
+import UIKit
+public class CrazyCell: UITableViewCell{
+    
+    @IBOutlet weak var AuthorOutlet: UILabel!
+    @IBOutlet weak var TitleOutlet: UILabel!
+    @IBOutlet weak var ChaptersOutlet: UILabel!
+    @IBOutlet weak var PagesOutlet: UILabel!
+  
+}
--- TableViewNotes/ViewController.swift
+++ TableViewNotes/ViewController.swift
@@ -5,15 +5,63 @@
 //  Created by JAKE KENEFICK on 10/18/23.
 //
 
+struct Book{
+    var author : String
+    var title : String
+    var pages : Int
+    var chapters : Int
+}
+
+
+
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+   
+    
+    @IBOutlet weak var TextField: UITextField!
+    
+    
+    
+    @IBOutlet weak var tableViewOutley: UITableView!
+    var numbers = [3,5,7,9,12,15]
+    var books = [Book]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableViewOutley.delegate = self
+        tableViewOutley.dataSource = self
+        books.append(Book(author: "Seaver", title: "Figure it out", pages: 12, chapters: 2))
+        books.append(Book(author: "Steven", title: "Psycho", pages: 5, chapters: 10))
         // Do any additional setup after loading the view.
     }
-
-
+   
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return books.count
+    }
+    //This populates each cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")
+       as! CrazyCell
+        cell.textLabel?.text =  "\( numbers[indexPath.row])"
+        cell.detailTextLabel?.text = "Hello"
+        cell.AuthorOutlet.text = books[indexPath.row].author
+        cell.TitleOutlet.text = books[indexPath.row].title
+        cell.PagesOutlet.text = "\(books[indexPath.row].pages)"
+        cell.ChaptersOutlet.text = "\(books[indexPath.row].chapters)"
+        return cell
+    }
+    
+    @IBAction func AddAction(_ sender: UIBarButtonItem) {
+     var blah = TextField.text!
+        var num = Int(blah)
+        if let n = num{
+            numbers.append(n)
+            tableViewOutley.reloadData()
+        }
+    }
+    
 }
 
